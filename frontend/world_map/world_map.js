b665f8c86275d3a91e632d26a79ddc0eb09ce55b
aleksander <aleksander.hrastic@ltfe.org>
2021-11-07 00:55:23 +0100

    worldmap

--- frontend/world_map/world_map.js
+++ frontend/world_map/world_map.js
@@ -1,9 +1,9 @@
-var mymap = L.map('worldmapid').setView([51.505, -0.09], 13);
+var mymap = L.map('wmap').setView([46.056946, 14.505751], 13);
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     id: 'mapbox/streets-v11',
     tileSize: 512,
     zoomOffset: -1,
-    accessToken: 'your.mapbox.access.token'
+    accessToken: 'pk.eyJ1IjoiY2hvZXN5IiwiYSI6ImNrdm9nODg3NDEwenMycXFmZDh2dWpqcmQifQ.xG91AESpamTmUy57bP9mtw'
 }).addTo(mymap);
