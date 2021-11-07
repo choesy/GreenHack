26a747cde3b49d5ca10242505d29a36247a950f4
Aljaz V <aljaz.verlic@gmail.com>
2021-11-07 12:11:26 +0100

    chart

--- frontend/src/views/TopStories.vue
+++ frontend/src/views/TopStories.vue
@@ -1,7 +1,12 @@
 <template>
 
   <div>
-    <NewsList :newsArticles="newsArticles"></NewsList>
+    <GChart
+    type="PieChart"
+    :data="chartData"
+    :options="chartOptions"
+    />
+
   </div>
 
 </template>
@@ -11,14 +16,43 @@ import { Component, Vue } from 'vue-property-decorator';
 import newsService from '../services/newsService';
 import NewsList from '../components/NewsList.vue';
 import { ArticleType, NewsArticle } from '@/types';
+import { GChart } from 'vue-google-charts';
+
 
 @Component({
   components: {
-    NewsList
+    NewsList,
+    GChart
   }
 })
 export default class TopStories extends Vue {
   newsArticles: NewsArticle[] = [];
+
+   data () {
+    return {
+      // Array will be automatically processed with visualization.arrayToDataTable function
+      chartData: [
+          ['Task', 'Hours per Day'],
+          ['CO2 saved',     8],
+          ['Gray',      2],
+          ['Transparent',  2],
+
+      ],
+      chartOptions: {
+          title: "Co2 stat",
+          pieHole: 0.8,
+          legend: 'none',
+          slices: {
+            0: { color: 'green' },
+            1: { color: 'gray' },
+            2: { color: 'transparent' }
+          },
+          pieStartAngle: 210,
+
+
+      }
+    }
+  }
 
   mounted () {
     newsService.getArticlesByType(ArticleType.TopStory)
